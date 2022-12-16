a27a0566daba1b959a7115793ef5b8483c432116
CerNico <79951240+CerNico@users.noreply.github.com>
2022-12-16 03:02:45 +0100

    first

---
+++ bg/schemas/project.js
@@ -0,0 +1,38 @@
+export default {
+    name: 'project',
+    title: 'Prosjekt',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Tittel',
+            type: 'string'
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLenght: 36,
+            }
+        },
+        {
+            name: 'cover',
+            title: 'Cover',
+            type: 'image',
+        },
+        {
+            name: 'gallery',
+            title: 'Bildegalleri',
+            type: 'array',
+            of: [{type:'image'}],
+        },
+        {
+            name: 'process',
+            title: 'Prosess',
+            type: 'blockContent',
+        }
+        
+    ]
+}
