a32c6610e6cc9f6a21a76789b3ed3911d79af471
CerNico <79951240+CerNico@users.noreply.github.com>
2022-12-16 05:31:17 +0100

    code

---
+++ front/js/env.js
@@ -0,0 +1,5 @@
+export const projectID = 'vuzhqgou';
+// address for web api to sanity
+export const sanityUrl = `https://${projectID}.api.sanity.io/v2021-03-25/data/query/production?query=`;
+// cdn address where sanity save our assets 
+export const cdnUrl = `https://cdn.sanity.io/images/${projectID}/production/`;
